d26f67f07a7a2fda9b7487127fc365380b597139
error-Tawhid <snburgs4@gmail.com>
2025-09-18 23:55:51 +0600

    useRef

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
-import React, { useEffect, useState } from "react";
-import Counter from "./components/Counter";
+import { useRef } from "react";
 
 const App = () => {
+  const inputElement = useRef(null);
+
+  const Handlefocus = () => {
+    inputElement.current.focus();
+    inputElement.current.value = "Tawhid";
+    console.log(inputElement.current);
+  };
   return (
     <>
       <h1>App</h1>
-      <Counter />
+      <input type="text" ref={inputElement} />
+      <button onClick={() => Handlefocus()}>click me</button>
     </>
   );
 };
