01bd66451d15074d5ef80145ea27c985dd101660
error-Tawhid <snburgs4@gmail.com>
2025-09-20 14:14:20 +0600

    clean

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
-import { useFetch } from "./Hooks/useFetch";
-
 const App = () => {
-  const [data] = useFetch("https://jsonplaceholder.typicode.com/todos");
   return (
     <>
       <h1>App</h1>
-      {data && data.map((item) => <p key={item.id}>{item.title}</p>)}
     </>
   );
 };
